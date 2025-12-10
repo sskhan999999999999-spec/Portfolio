e68a1cca299050c08cecddfc364ca61aa1ab1f5d
sskhan999999999999@gmail.com <sskhan@gmail.com>
2025-12-10 13:38:11 +0500

    new changes about mobile work section

--- src/app/Components/HomePages/Work.jsx
+++ src/app/Components/HomePages/Work.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Work() {
   return (
     <div>
-                    <section className="text-center py-20 bg-gray-50 dark:bg-indigo-200">
+                    <section className="text-center py-20 bg-gray-50 dark:bg-indigo-200 px-5">
                 <h2 className="text-5xl font-bold mb-4 bg-linear-to-r from-purple-600 to-indigo-600 bg-clip-text text-transparent p-2">Let’s Work Together</h2>
                 <p className="text-gray-600  max-w-xl mx-auto mb-6">
                     I’m open to freelance projects, collaborations, and full-time roles.
